b3ed6a0bb8a59f0bc126abbd7b1579e1a7655a1a
JOAO VICTOR NUNES DA SILVA <joao.nunes@nti.ufal.br>
2019-11-01 15:50:48 -0300

    upando

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from './component'
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
