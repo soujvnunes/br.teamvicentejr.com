5d890924371cfb7ac7a74fac633ef9fb59ab535d
JOAO VICTOR NUNES DA SILVA <joao.nunes@nti.ufal.br>
2019-11-01 22:25:04 -0300

    Estruturação de cabeçalho.

---
+++ src/app/component/Link/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default (props) => (
+	<a className={[
+		props.className,
+		"link"
+	].join(' ')} href={props.href}>{props.children}</a>
+)
--- src/app/component/Nav/index.js
+++ src/app/component/Nav/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from "../../component";
 
 export default (props) => (
-	<nav className="">
-		<a>Professores</a>
-		<a>Sobre</a>
-		<a>Eventos</a>
+	<nav className="nav">
+		<Link className="nav-link" href="/">Professores</Link>
+		<Link className="nav-link" href="/">Sobre</Link>
+		<Link className="nav-link" href="/">Eventos</Link>
 	</nav>
 )
