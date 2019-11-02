407e21d6970d8395f162baa4a538357f3a144e7d
JOAO VICTOR NUNES DA SILVA <joao.nunes@nti.ufal.br>
2019-11-01 22:28:20 -0300

    Reformulação de <Link />

--- src/app/component/Link/index.js
+++ src/app/component/Link/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 export default (props) => (
 	<a className={[
-		props.className,
-		"link"
-	].join(' ')} href={props.href}>{props.children}</a>
+			props.className,
+			"link"
+		].join(' ')}
+	   href={props.href}
+	>
+		{props.children}
+	</a>
 )
