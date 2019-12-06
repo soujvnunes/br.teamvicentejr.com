765b88735dcfcb79561d9c655f72cff9ab20037d
10893037427 <joao.nunes@nti.ufal.br>
2019-12-06 17:51:12 -0300

    ajustes 20191206

--- frontend/src/component/Line/index.js
+++ frontend/src/component/Line/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Button from "../Button";
 import Icon from "../Icon";
+import Typography from "../Typography";
 
 export default props => (
   <li className={"Line"}>
     <div className={"Line-info"}>
-      <h4 className={"Line-info-title"}>Nome do evento</h4>
-      <p className={"Line-info-description"}>
-        <Icon name={"local"} />
-        Local
-      </p>
+      <Typography subject={"Nome do evento"} className={"Line-info-title"} />
+      <div className={"Line-info-local"}>
+        <Icon name={"local"} className={"Line-info-local-icon"} />
+        <Typography subject={"Local"} className={"Line-info-local-label"} />
+      </div>
     </div>
     <Button
       id={"open-event"}
