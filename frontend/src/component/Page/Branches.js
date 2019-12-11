2e2f6fefdbf5020f208c37af6bf3e2810558325a
10893037427 <joao.nunes@nti.ufal.br>
2019-12-11 18:28:49 -0300

    ajustes 20191211

--- frontend/src/component/Page/Branches.js
+++ frontend/src/component/Page/Branches.js
@@ -4,6 +4,10 @@ import Description from "../Description";
 import Typography from "../Typography";
 import Button from "../Button";
 import Section from "../Section";
+import Map from "../Map";
+import { withGoogleMap, withScriptjs } from "react-google-maps";
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export default props => {
   let { event, switchContent } = props;
@@ -29,7 +33,14 @@ export default props => {
             state={switchContent && "expanded"}
           />
         </Description>
-        <Section show={!switchContent}></Section>
+        <Section show={!switchContent}>
+          <MapWrapped
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBOY6UqLwmYbrczni-guS7b0KPnYfG-uPg`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        </Section>
         <Section show={switchContent}>
           <Typography paragraph>imagem das filiais</Typography>
         </Section>
