ddc556228e71ced8e147c7f36f29802b448205a7
JOÃO VICTOR NUNES DA SILVA <soujvnunes@gmail.com>
2020-01-03 21:29:49 -0300

    ajustes de responsividade

--- frontend/src/component/Nav/index.js
+++ frontend/src/component/Nav/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-/*
 import { useMediaQuery } from "react-responsive";
-*/
 import classNames from "classnames";
 import IconButton from "../IconButton";
 import Skew from "../Skew";
 import Mark from "../../asset/mark.png";
 
 export default props => {
-  /*  const isPhone = useMediaQuery({ maxDeviceWidth: 600 });*/
+  const isPhone = useMediaQuery({ maxDeviceWidth: 600 });
   let { children, indicator } = props;
 
   const classes = {
@@ -29,29 +27,33 @@ export default props => {
         <Skew contained className={runner} />
         {children}
       </div>
-      <a href={"/"} className={"Nav-logo"}>
-        <img src={Mark} alt={"marca da aplicação"} />
-      </a>
-      <aside className={"Nav-footer"}>
-        <IconButton
-          icon={"instagram"}
-          url={"https://instagr.am/vicentejrteambrasil"}
-          noHover={true}
-          className={"Nav-footer-link"}
-        />
-        <IconButton
-          icon={"phone"}
-          url={"mailto:vicentejrteam@gmail.com?subject=Contato pelo site"}
-          noHover={true}
-          className={"Nav-footer-link"}
-        />
-        <IconButton
-          icon={"youtube"}
-          url={"https://youtube.com"}
-          className={"Nav-footer-link"}
-          noHover={true}
-        />
-      </aside>
+      {!isPhone && (
+        <>
+          <a href={"/"} className={"Nav-logo"}>
+            <img src={Mark} alt={"marca da aplicação"} />
+          </a>
+          <aside className={"Nav-footer"}>
+            <IconButton
+              icon={"instagram"}
+              url={"https://instagr.am/vicentejrteambrasil"}
+              noHover={true}
+              className={"Nav-footer-link"}
+            />
+            <IconButton
+              icon={"phone"}
+              url={"mailto:vicentejrteam@gmail.com?subject=Contato pelo site"}
+              noHover={true}
+              className={"Nav-footer-link"}
+            />
+            <IconButton
+              icon={"youtube"}
+              url={"https://youtube.com"}
+              className={"Nav-footer-link"}
+              noHover={true}
+            />
+          </aside>
+        </>
+      )}
     </nav>
   );
 };
