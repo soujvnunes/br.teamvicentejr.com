5fee857eccabef005a0c9958da41c6999948ba3a
JOÃO VICTOR NUNES DA SILVA <soujvnunes@gmail.com>
2020-01-07 23:11:28 -0300

    instalação de plgs para trandução e ajustes no componente de legenda do slider dos professores

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import App from "./component/App";
 import "./index.scss";
 import * as serviceWorker from "./util/serviceWorker";
 import { ReactComponent as Icon } from "./asset/icon.svg";
+import "./i18n";
 
 ReactDOM.render(<Icon />, document.getElementById("icon"));
 ReactDOM.render(<App />, document.getElementById("root"));
