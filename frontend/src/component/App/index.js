ffd26a8971a88b9736292043930106dfcf9f9cdd
JOÃO VICTOR NUNES DA SILVA <soujvnunes@gmail.com>
2020-01-12 16:09:58 -0300

    reformulação do Header com styled components

--- frontend/src/component/App/index.js
+++ frontend/src/component/App/index.js
@@ -4,8 +4,9 @@ import { data } from "../../lib/data";
 import Nav from "../Nav";
 import { Home, Teachers, Branches, About } from "../Page";
 import NavItem from "../NavItem";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyle from "../../util/globalStyle";
+import { theme } from "../../util/theme";
 
 const AppWrapper = styled.section`
   display: flex;
@@ -22,27 +23,29 @@ export default function App() {
   useEffect(() => setState({ active: "home" }), []);
 
   return (
-    <Router>
-      <GlobalStyle />
-      <AppWrapper>
-        <Nav indicator={active}>
-          {data.navigation.map((data, i) => (
-            <NavItem
-              key={i}
-              to={data.to}
-              isActive={() => active === data.name}
-              onClick={() => setState({ ...state, active: `${data.name}` })}
-              primary={data.primary}
-              icon={data.icon}
-              iconActive={active === data.name}
-            />
-          ))}
-        </Nav>
-        <Route exact path="/" render={props => <Home />} />
-        <Route exact path="/professores" render={props => <Teachers />} />
-        <Route exact path="/filiais" render={props => <Branches />} />
-        <Route exact path="/sobre" render={props => <About />} />
-      </AppWrapper>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyle />
+        <AppWrapper>
+          <Nav indicator={active}>
+            {data.navigation.map((data, i) => (
+              <NavItem
+                key={i}
+                to={data.to}
+                isActive={() => active === data.name}
+                onClick={() => setState({ ...state, active: `${data.name}` })}
+                primary={data.primary}
+                icon={data.icon}
+                iconActive={active === data.name}
+              />
+            ))}
+          </Nav>
+          <Route exact path="/" render={props => <Home />} />
+          <Route exact path="/professores" render={props => <Teachers />} />
+          <Route exact path="/filiais" render={props => <Branches />} />
+          <Route exact path="/sobre" render={props => <About />} />
+        </AppWrapper>
+      </Router>
+    </ThemeProvider>
   );
 }
--- frontend/src/component/Header/index.js
+++ frontend/src/component/Header/index.js
@@ -1,35 +1,46 @@
 import React from "react";
-import classNames from "classnames";
 import Wrapper from "../Wrapper";
 import Typography from "../Typography";
+import styled, { css } from "styled-components";
+import { down } from "styled-breakpoints";
+
+const HeaderRoot = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  background-color: var(--color-black);
+  padding: calc(var(--spacing) * 14px) 0 calc(var(--spacing) * 2px) 0;
+  min-height: ${props =>
+    props.image ? "calc(var(--spacing) * 100px)" : "auto"};
+  text-align: center;
+  position: relative;
+  box-shadow: inset 0 calc(var(--spacing) * -24px) calc(var(--spacing) * 8)
+    calc(var(--spacing) * -8px) var(--color-black);
+  ${props =>
+    props.image &&
+    css`
+      background-image: url(${props.image});
+    `}
+
+  ${down("md")} {
+    min-height: 50vh;
+    padding-top: calc(var(--spacing) * 9px);
+  }
+`;
 
 export default function Header(props) {
-  let { className, image, title, subject, align } = props;
-
-  const classes = {
-    root: [
-      classNames(
-        "Header",
-        { [`Header--align-${align}`]: align },
-        { [`Header--noImage`]: !image },
-        className
-      )
-    ]
-  };
+  let { image, title, subject } = props;
 
   return (
-    <header
-      className={classes.root}
-      style={image ? { backgroundImage: `url(${image})` } : null}
-    >
-      <Wrapper className={"Header-wrapper"}>
-        <Typography variant={"title"} className={"Header-title"}>
-          {title}
-        </Typography>
-        <Typography variant={"subtitle"} className={"Header-subject"}>
-          {subject}
-        </Typography>
+    <HeaderRoot image={image}>
+      <Wrapper>
+        <Typography variant={"title"}>{title}</Typography>
+        <Typography variant={"subtitle"}>{subject}</Typography>
       </Wrapper>
-    </header>
+    </HeaderRoot>
   );
 }
